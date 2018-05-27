139450d6c3f2396862b5ed9e4e3cf5869d569b85
ATarsa <smb909@gmail.com>
2018-05-27 19:19:24 +0100

    Added function to update myLibrary list with a new book

--- scripts.js
+++ scripts.js
@@ -19,21 +19,48 @@ const year1984 = new Book("year1984", "G.Orwell", 333, false);
 addBookToLibrary(year1984)
 const harryPotter = new Book("Harry Potter", "JKR", 400, true);
 addBookToLibrary(harryPotter)
-console.log(myLibrary);
+console.table(myLibrary);
 
 function render(){
+
     const table = document.querySelector('table');
+    table.innerHTML = "";
     
     
     for (let i = 0; i < myLibrary.length; i++){
         table.innerHTML += '<tr>' + '<td>' + myLibrary[i].title + '</td>'+ '<td>' + myLibrary[i].author + '</td>' + '<td>' + myLibrary[i].pages + '</td>' + '<td>' + myLibrary[i].read + '</td>' + '</tr>';
     }
 }
-render();
 
-// add new book form
+
+// toggle new book form
 const newBookBtn = document.querySelector('button');
 newBookBtn.onclick = function(){
     const newBookForm = document.querySelector('#newBookForm');
     newBookForm.classList.toggle('hidden');
 }
+
+// update myLibrary list with a new book
+const addBookBtn = document.querySelector('#addBook');
+
+addBookBtn.onclick = function(){
+   let title = document.querySelector('[name="title"]');
+   let author = document.querySelector('[name="author"]');
+   let pages = document.querySelector('[name="pages"]');
+   let read = document.querySelector('[name="read"]');
+
+   let newRecord = new Book(title.value, author.value, pages.value, read.value);
+
+   addBookToLibrary(newRecord);
+   //clean form's inputs
+   title.value = "";
+   author.value = "";
+   pages.value = "";
+   read.value = "";
+     
+   render();
+
+   newBookForm.classList.add('hidden');
+
+}
+render();
